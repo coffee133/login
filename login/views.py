c13bdb7dee5cb2e9675830637c2df348d9704a88
coffee <coffee.qiao@shaoke.com>
2024-05-11 13:03:35 +0800

    0511,add

--- login/views.py
+++ login/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from . import models
+from . import forms
 # Create your views here.
 
 def index(request):
@@ -8,10 +10,23 @@ def index(request):
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        print(username, password)
-        return redirect('/index/')
+        login_form = forms.UserForm(request.POST)
+        message = '请检查填写的内容！'
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+            try:
+                user = models.User.objects.get(name=username)
+            except:
+                message = '用户不存在！'
+                return render(request, 'login/login.html', locals())
+            if user.password == password:
+                return redirect('/index/')
+            else:
+                message = '密码不正确！'
+                return render(request, 'login/login.html', locals())
+        else:
+            return render(request, 'login/login.html', locals())
     return render(request, 'login/login.html')
 
 def register(request):
